7ce4ae57cb2f57938ba42fc9448583858414f880
xorphitus <xorphitus@gmail.com>
2024-08-28 20:30:19 +0900

    refactor

--- main.py
+++ main.py
@@ -10,9 +10,30 @@ SPEAKER = "1"
 def is_process_running(process_name):
     return True
 
+def play(text):
+    encoded_text = urllib.parse.quote(text)
+    audio_query_response = requests.post(f"http://localhost:50021/audio_query?speaker={SPEAKER}&text={encoded_text}")
+    if audio_query_response.status_code != 200:
+        print("Failed to get audio query.")
+        print(audio_query_response)
+        return False
+
+    audio_query_response = audio_query_response.json()
+    audio_query_response['speedScale'] *= 1.4
+
+    response = requests.post(f"http://localhost:50021/synthesis?speaker={SPEAKER}", json=audio_query_response)
+    if response.status_code != 200:
+        print("Failed to get WAV.")
+        print(response)
+        return False
+
+    process = subprocess.Popen(["aplay"], stdin=subprocess.PIPE)
+    process.communicate(input=response.content)
+
+    return True
+
 def main():
     while True:
-        # 1. Check if 'voicevox' and 'ollama' processes are running
         if not is_process_running('voicevox') or not is_process_running('ollama'):
             print("Required processes are not running.")
             return
@@ -38,21 +59,7 @@ def main():
             print(response.json())
             continue
 
-        encoded_content = urllib.parse.quote(reply)
-        audio_query_response = requests.post(f"http://localhost:50021/audio_query?speaker={SPEAKER}&text={encoded_content}")
-        if audio_query_response.status_code != 200:
-            print("Failed to get audio query.")
-            print(audio_query_response)
-            continue
-
-        response = requests.post(f"http://localhost:50021/synthesis?speaker={SPEAKER}", json=audio_query_response.json())
-        if response.status_code != 200:
-            print("Failed to get WAV.")
-            print(response)
-            continue
-
-        process = subprocess.Popen(["aplay"], stdin=subprocess.PIPE)
-        process.communicate(input=response.content)
+        play(reply)
 
 if __name__ == "__main__":
     main()
